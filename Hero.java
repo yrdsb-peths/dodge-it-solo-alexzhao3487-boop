e0fdd290d7ce7033745df6dca06ae3572d8340ff
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-04-14 16:33:57 -0400

    Hero vertical movement
    moves up and down on click

--- Hero.java
+++ Hero.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Hero here.
+ * A hero 
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,8 +12,19 @@ public class Hero extends Actor
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    boolean atTop = true;
+    
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null)) {
+            atTop = !atTop;
+        }
+        
+        if(atTop) {
+            setLocation(100, 100);
+        }
+        else {
+            setLocation(100, 300);
+        }
     }
 }
